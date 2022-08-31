67ca77b79de81b6298f22dc92112dc75e9fc2f09
kionou <kionoumamadou.00@gmail.com>
2022-08-31 23:44:06 +0000

    fin

--- server/data/database.js
+++ server/data/database.js
@@ -1,16 +1,30 @@
 
 const  Sequelize  = require("sequelize")
 
-let sequelize = new Sequelize("sante_aout","postgres","nan2021",{
-    dialect:'postgres',
-    host:'localhost',
-    port: 5432
-})
-
-// let sequelize = new Sequelize("sante_aout","postgres","Laloi2015",{
+// let sequelize = new Sequelize("sante_aout","postgres","nan2021",{
 //     dialect:'postgres',
 //     host:'localhost',
-//     port:'5432'
+//     port: 5432
+// })
+
+let sequelize = new Sequelize("sante_aout","postgres","Laloi2015",{
+    dialect:'postgres',
+    host:'localhost',
+    port:'5432'
+})
+
+
+// let sequelize = new Sequelize(process.env.DATABASE_URL,{
+//     dialect:'postgres',
+//     protocol: 'postgres',
+//      dialectOptions: {
+//          ssl: {
+//              require: true,
+//              rejectUnauthorized: false
+//          }
+//      }
+//     // host:'localhost',
+//     // port:'5432'
 // })
 
 
